86a986a678ef4dfaa05c8dcfa4a3a786b0f43a12
chick <chick@eecs.berkeley.edu>
2014-01-31 16:50:28 -0800

    reformatted pyast.py indentation and other pycharm warnings

--- hindemith/hindemith.py
+++ hindemith/hindemith.py
@@ -1,14 +1,15 @@
-# Python matrix/vector data and routines
-from numpy import *
-from hlib import *
-import scipy.sparse
-import asp
-from asp.jit.asp_module import ASPModule
+"""
+Hindemith specializer entry point
+Uses the @hm function annotation to
+parse, analyze and run python code
+using openCL
+"""
 
-# AST manipulation
-import ast
+__author__ = "Michael J. Anderson"
+
+from hlib import *
+from asp.jit.asp_module import ASPModule
 import inspect
-import copy
 
 # h compiler
 import PyAST 
@@ -16,20 +17,31 @@ import PyAST
 state = {}
 defines = {}
 
-class hm_fn:
+
+class HindemithSpecializer:
     def __init__(self, fn):
         self.fn = fn
         self.source = inspect.getsource(fn)
+        self.specializer = Hindemith()
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, **kwargs):
         global state
         global defines
         (record_id, new_kwargs) = PyAST.mycompile(self.source, kwargs, defines)
         (return_value, state) = PyAST.run_backend(record_id, new_kwargs)
         return return_value
 
+
 def hm(fn):
-  return hm_fn(fn)
+    """
+    creates the hm annotation which is hook by which this specializer
+    is started, usage
+    @hm
+    def function_to_be_specialized()
+
+    """
+    return HindemithSpecializer(fn)
+
 
 class Hindemith(object):
     """
@@ -37,4 +49,3 @@ class Hindemith(object):
     """
     asp_module = ASPModule("hindemith")
     asp_module.validate()
-
