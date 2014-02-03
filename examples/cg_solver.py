b955765b5baf7dc013a3eaf3684044b170311ba4
chick <chick@eecs.berkeley.edu>
2014-02-02 22:33:20 -0800

    Reformat basic example python

--- examples/cg_solver.py
+++ examples/cg_solver.py
@@ -1,30 +1,32 @@
 # CG solver using s-step specializer
 
-from numpy import *
-import scipy.sparse
+# from numpy import *
+# import scipy.sparse
 
 from hindemith.hindemith import *
 
+
 def setup_hindemith():
-  defines['nd1'] = 1
-  defines['nt1'] = 128
-  defines['ne1'] = 2
+    defines['nd1'] = 1
+    defines['nt1'] = 128
+    defines['ne1'] = 2
+
 
 @hm
 def cg_solver(A, b, x, sc):
-  r = b - A*x
-  p = b - A*x
-  rsold = sum(r * r)
-#  residual = b - A*x
-#  rnorm = sum(residual*residual)
-#  while(rnorm > 20.0):
-  for i in range(400):
-    Ap = A * p
-    alpha = rsold / sum(p * Ap)
-    x = x + alpha * p
-    r = r - alpha * Ap
-    rsnew = sum(r * r)
-    beta = rsnew / rsold
-    p = r + beta * p
-    rsold = rsnew
-  return x
+    r = b - A * x
+    p = b - A * x
+    rsold = sum(r * r)
+    #  residual = b - A*x
+    #  rnorm = sum(residual*residual)
+    #  while(rnorm > 20.0):
+    for i in range(400):
+        Ap = A * p
+        alpha = rsold / sum(p * Ap)
+        x = x + alpha * p
+        r = r - alpha * Ap
+        rsnew = sum(r * r)
+        beta = rsnew / rsold
+        p = r + beta * p
+        rsold = rsnew
+    return x
--- examples/test_cg.py
+++ examples/test_cg.py
@@ -1,6 +1,6 @@
 __author__ = 'Michael J. Anderson'
 
-from numpy import *
+# from numpy import *
 import scipy.sparse
 from hindemith.hindemith import *
 
@@ -17,38 +17,38 @@ d2 = array([1.0] * dim1 * dim0, dtype=float32)
 d3 = array([0.4] * dim1 * dim0, dtype=float32)
 d4 = array([0.1] * dim1 * dim0, dtype=float32)
 
-d0.shape = (dim1,dim0)
-d1.shape = (dim1,dim0)
-d2.shape = (dim1,dim0)
-d3.shape = (dim1,dim0)
-d4.shape = (dim1,dim0)
+d0.shape = (dim1, dim0)
+d1.shape = (dim1, dim0)
+d2.shape = (dim1, dim0)
+d3.shape = (dim1, dim0)
+d4.shape = (dim1, dim0)
 
-d0[0,:] = 0.0
-d1[:,0] = 0.0
-d3[:,dim0-1] = 0.0
-d4[dim1-1,:] = 0.0
+d0[0, :] = 0.0
+d1[:, 0] = 0.0
+d3[:, dim0 - 1] = 0.0
+d4[dim1 - 1, :] = 0.0
 
-d0.shape = (dim1*dim0,)
-d1.shape = (dim1*dim0,)
-d2.shape = (dim1*dim0,)
-d3.shape = (dim1*dim0,)
-d4.shape = (dim1*dim0,)
+d0.shape = (dim1 * dim0,)
+d1.shape = (dim1 * dim0,)
+d2.shape = (dim1 * dim0,)
+d3.shape = (dim1 * dim0,)
+d4.shape = (dim1 * dim0,)
 data = [d4, d3, d2, d1, d0]
 
-A = scipy.sparse.spdiags(data,[-dim0,-1,0,1,dim0],dim1*dim0,dim1*dim0)
+A = scipy.sparse.spdiags(data, [-dim0, -1, 0, 1, dim0], dim1 * dim0, dim1 * dim0)
 
-b = 0.5*ones((dim1*dim0,), order='F', dtype=float32)
+b = 0.5 * ones((dim1 * dim0,), order='F', dtype=float32)
 random.seed(5)
-b[:] = random.rand(dim1*dim0)
-x = 0.5 * ones((dim1*dim0,), order='F', dtype=float32)
+b[:] = random.rand(dim1 * dim0)
+x = 0.5 * ones((dim1 * dim0,), order='F', dtype=float32)
 
 residual_before = sqrt(dot(b - A * x, b - A * x))
-z = cg_solver.cg_solver(A=A,b=b,x=x, sc=float32(2.0))
+z = cg_solver.cg_solver(A=A, b=b, x=x, sc=float32(2.0))
 residual_after = sqrt(dot(b - A * z, b - A * z))
 
 if (residual_after / residual_before) < 0.02:
-  #print 'PASSED: ' + str(residual_before)  + ', ' + str(residual_after)
-  print 'PASSED'
+    #print 'PASSED: ' + str(residual_before)  + ', ' + str(residual_after)
+    print 'PASSED'
 else:
-  #print 'FAILED: ' + str(residual_before)  + ', ' + str(residual_after)
-  print 'FAILED'
+    #print 'FAILED: ' + str(residual_before)  + ', ' + str(residual_after)
+    print 'FAILED'
